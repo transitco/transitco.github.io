55fbccfd2df58c90253e21a5dc5fc89fc8fefa7f
Francis Peixoto <peixoto.francis@gmail.com>
2020-04-22 17:15:04 -0400

    switched to tracking milestones

--- app/routes/roadmap.js
+++ app/routes/roadmap.js
@@ -2,9 +2,9 @@ import Route from '@ember/routing/route';
 
 export default class RoadmapRoute extends Route {
     async model(){
-        const response = await fetch('https://api.github.com/repos/transitco/roadmap/issues');
-        const issues = await response.json();
+        const response = await fetch('https://api.github.com/repos/transitco/roadmap/milestones');
+        const milestones = await response.json();
 
-        return {issues};
+        return {milestones};
     }
 }
